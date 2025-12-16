7c5403608d640e72c88b64d42206e8dc7a8eddf0
Victor <victorariel.rcutlook.com>
2025-12-15 22:24:38 -0300

    feat: configuración de conexión a SQL Server y migración inicial aplicada

---
+++ Datos/GestorContaxtosDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Datos
+{
+    public class GestorContactosDbContextFactory : IDesignTimeDbContextFactory<GestorContactosDbContext>
+    {
+        public GestorContactosDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<GestorContactosDbContext>();
+
+            // Conexión a SQL Server en localhost con autenticación de Windows
+            optionsBuilder.UseSqlServer("Server=localhost;Database=GestorContactosDBs;Trusted_Connection=True;Encrypt=False;");
+
+
+            return new GestorContactosDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Datos/Migrations/20251216011216_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Direcciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Calle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Direcciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contactos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rut = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DireccionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contactos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contactos_Direcciones_DireccionId",
+                        column: x => x.DireccionId,
+                        principalTable: "Direcciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contactos_DireccionId",
+                table: "Contactos",
+                column: "DireccionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contactos");
+
+            migrationBuilder.DropTable(
+                name: "Direcciones");
+        }
+    }
+}
